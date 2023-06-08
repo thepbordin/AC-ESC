197c1ceb6496f20bb48c278a7c27ebfc6611e7c4
RatchanonMo <49874@yupparaj.ac.th>
2023-06-09 02:40:37 +0700

    eiei

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
         <div className="bg-[#E7E9F3] p-1 rounded-full flex font-bold">
           <p className=" bg-white px-3 py-2 rounded-full">หน้าแรก</p>
           <p className="mx-5 px-3 py-2 rounded-full">หน้าแรก</p>
-        </div>
+        </div> 
         <div></div>
       </div>
     </div>
