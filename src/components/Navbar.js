f46b0aac7aa07c6ced6c4de1b9a472f12960c28e
RatchanonMo <49874@yupparaj.ac.th>
2023-06-09 02:56:22 +0700

    completed but need logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  //navbar scroll when active state
+  const [navbar, setNavbar] = useState("");
+
+  //navbar scroll changeBackground function
+  const changeBackground = () => {
+    // console.log(window.scrollY);
+    if (window.scrollY >= 66) {
+      setNavbar("bg-white");
+    } else {
+      setNavbar("false");
+    }
+  };
+
+  useEffect(() => {
+    changeBackground();
+    // adding the event when scroll change background
+    window.addEventListener("scroll", changeBackground);
+  });
+
   return (
     <div>
-      <div className="flex justify-between">
+      <div className={`${navbar} fixed w-full p-4 flex justify-between`}>
         <div></div>
-        <div className="bg-[#E7E9F3] p-1 rounded-full flex font-bold">
-          <p className=" bg-white px-3 py-2 rounded-full">หน้าแรก</p>
-          <p className="mx-5 px-3 py-2 rounded-full">หน้าแรก</p>
-        </div> 
-        <div></div>
+        <div className="bg-[#E7E9F3] p-2 rounded-full flex font-bold">
+          <Link to="/" className=" bg-white px-3 py-2 rounded-full text-[#2a384a]">
+            หน้าแรก
+          </Link>
+          <Link to="" className="mx-5 px-3 py-2 rounded-full text-[#2a384a]">
+            การนำเสนอด้วยวาจา
+          </Link>
+          <Link to="" className="mx-5 px-3 py-2 rounded-full text-[#2a384a]">
+            การนำเสนอด้วยโปสเตอร์
+          </Link>
+          <Link to="" className="mx-5 px-3 py-2 rounded-full text-[#2a384a]">
+            เกี่ยวกับงาน
+          </Link>
+        </div>
+        <div className="bg-blue-500 p-2 rounded-full flex font-extrabold">
+          <Link to="/" className="  px-3 py-2 rounded-full text-white">
+            สมัครเลย !
+          </Link>
+       
+        </div>
       </div>
     </div>
   );
