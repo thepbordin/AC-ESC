2f8649656daa4e69b0134a5b37102246a1671faa
Thepbordin Jaiinsom <thepbordinjaiinsom@gmail.com>
2023-06-10 00:24:31 +0700

    add timeline section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-
+import Timeline from "../components/Timeline";
 
 
 export default function Home() {
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
-      
+      <Timeline></Timeline>
     </>
   );
 }
