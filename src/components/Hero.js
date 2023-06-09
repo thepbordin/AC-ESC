041ab0b3cbd48f5a8447199c9a4af5b9fd753471
Thepbordin Jaiinsom <thepbordinjaiinsom@gmail.com>
2023-06-09 23:28:58 +0700

    hero & fix png gap

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,19 @@
 import React from "react";
 import ChangingText from "./ChangingText";
 export default function Hero() {
-  
-
   return (
     <>
-      <div className="flex justify-center flex-col text-center">
-        <img src="./title.png" alt="" />
-        <h1 className="font-bold text-3xl transition-all duration-300">
+      <div className="text-center pt-10">
+        <img
+          src="./title.png"
+          className="w-full max-w-[1000px] mx-auto"
+          alt=""
+        />
+        <h1 className="mt-10 font-bold text-4xl transition-all duration-300 mx-auto">
           งานแข่งขันโครงงาน
-          <span
-            className={`text-blue-500 font-bold`}
-          >
-            <ChangingText/>
-          </span>
+          <span className={'text-blue-500 font-bold'}><ChangingText /></span>
         </h1>
+        <span className="text-gray-400 block font-bold text-2xl mt-6 ">23-25 กรกฎาคม 2566</span>
       </div>
     </>
   );
