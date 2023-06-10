c1af63fe9b8125cb3ebafab7b1489bec973bfb2f
RatchanonMo <49874@yupparaj.ac.th>
2023-06-11 02:30:52 +0700

    login to changeStream for real-time

--- src/App.js
+++ src/App.js
@@ -3,8 +3,33 @@ import Home from "./pages/Home";
 
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
+import * as Realm from "realm-web";
+import { useEffect, useState } from "react";
+
+const app = new Realm.App({ id: "application-0-zwiku" });
 
 function App() {
+  const [user, setUser] = useState();
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const login = async () => {
+      // Authenticate anonymously
+      const user = await app.logIn(Realm.Credentials.anonymous());
+      setUser(user); // Connect to the database
+
+      const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+      const collection = mongodb.db("test").collection("project"); // Everytime a change happens in the stream, add it to the list of events
+
+      for await (const change of collection.watch()) {
+        setEvents((events) => [...events, change]);
+      }
+    };
+    login();
+
+    window.changeStream = events
+  }, []);
+
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
